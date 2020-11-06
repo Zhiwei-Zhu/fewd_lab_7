1c6a39232449c3b2e2aac5a203fe76e45b447dde
Vincent Zhu <zhu.zhiwei.vincent@gmail.com>
2020-11-06 16:37:52 +0000

    Update index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+let firstName = "Vincent";
+let favouriteColor = "black";
+var city ="Lubumbashi";
+var country = 'Congo';
+let favouriteFood = "egg fried rice";
+//2 distinct variable can't have the same name
+let city2 = "Paris";
+
+//console.log that I created just show on a terminal or console my variable  firstName, favouriteColor, favouriteFood and city
+console.log(firstName);
+console.log(favouriteColor);
+console.log(favouriteFood,city, city2);
+
+let phrase = "A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.";
+let endingPhrase = " is what we see. Rainbow colours, are made with light bending";
+let one = "Red";
+let two = "orange";
+let three = "yellow";
+let four = "green";
+let five = "blue";
+let six = "indigo";
+let seven = "violet";
+
+console.log(phrase+" "+one+", "+two+", "+three+", "+four+", "+five+", "+six+", "+seven+endingPhrase);
+
+function myAlphabetLength(array) {
+
+    console.log(array.length)
+
+}
+
